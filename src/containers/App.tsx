68e4b5d6ec698f2c7d33d9679d9f14bce6558144
aquaprogit <vladd.golovatyuk@gmail.com>
2022-08-17 19:06:57 +0300

    centered news line

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,31 +1,37 @@
 import { Stack } from "@mui/material";
 import NewsCardComponent from "../components/NewsCardComponent";
 import { fetchPosts } from "../components/NewsCardComponent";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { getPosts, PostData } from "../services/post";
-import React from 'react';
+import React from "react";
 
 function App() {
   const [posts, setPosts] = useState<PostData[]>([]);
   useEffect(() => {
     let mounted = true;
-    getPosts()
-      .then(items => {
-        if (mounted) {
-          setPosts(items)
-        }
-      });
+    getPosts().then((items) => {
+      if (mounted) {
+        setPosts(items);
+      }
+    });
   }, []);
-      return (
+  return (
     <div className="app">
       <div className="side-bar-container">
-            {posts.map(item => <NewsCardComponent body={item.body} title={item.title} src={'https://thumbs.dreamstime.com/b/news-paper-background-indoor-hand-made-taken-lahore-pakistan-171259538.jpg'} />)}
-      </div>
-      <div className="content">
-        <p>content gocontent goes herecontent goes herecontent goes herecontent goes herecontent goes herecontent goes hecontent goes herecontent goes herecontent goes herecontent goes herecontent goes herecontent goes hererecontent goes herecontent goes herecontent goes herecontent goes herecontent goes herees here</p>
+        <Stack spacing={5} className="stack">
+          {posts.map((item) => (
+            <NewsCardComponent
+              body={item.body}
+              title={item.title}
+              src={
+                "https://thumbs.dreamstime.com/b/news-paper-background-indoor-hand-made-taken-lahore-pakistan-171259538.jpg"
+              }
+            />
+          ))}
+        </Stack>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
